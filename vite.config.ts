e5ba72196e390dea0d85ce552365bcf4bebd1a91
EphraimShikanga <ephraimshikanga@gmail.com>
2025-11-25 06:15:05 +0300

    deploy again without gh

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,11 @@ import path from 'node:path'
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 import vue from '@vitejs/plugin-vue'
-import {ghPages} from "vite-plugin-gh-pages";
+// import {ghPages} from "vite-plugin-gh-pages";
 
 export default defineConfig({
   base: '/ecommerce/',
-  plugins: [vue(), tailwindcss(), ghPages()],
+  plugins: [vue(), tailwindcss()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
